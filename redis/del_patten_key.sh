7f6e7612f75a1d82a21231d179f998e6fb288a1d
hongtoushizi <yuanhongshuai3.11@163.com>
2016-05-20 00:54:11 +0800

    增加了redis带密码登录的功能

--- redis/del_patten_key.sh
+++ redis/del_patten_key.sh
@@ -13,3 +13,11 @@ echo  $pattern
 #redis-cli keys "ct_math*"  | xargs -p -I {} redis-cli del {}
 
 redis-cli keys "$pattern*"  | xargs -p -I {} redis-cli del {}
+
+# 带有密码的操作
+# redis-cli -a password keys “$pattern” | xargs redis-cli -a password del  
+
+
+
+#参考链接
+#http://blog.csdn.net/spring21st/article/details/15771861
