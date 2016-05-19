9de9dc3a17dbdcab0da5aec945200526e6b53d10
hongtoushizi <yuanhongshuai3.11@163.com>
2016-05-19 20:04:10 +0800

    test

--- centos/sshpass/loginByip.sh
+++ centos/sshpass/loginByip.sh
@@ -19,24 +19,24 @@ read CHOICE
   case $CHOICE in
     1) 
        echo  "test-math"
-       sshpass -p  'Pzh6537.!@#' ssh root@139.196.34.17 -p 10010
+       sshpass -p  '*****' ssh root@139.196.34.17 -p 10010
        ;;
     2) 
        echo  "算法服务器" 
-       sshpass -p  'hello2016!@#' ssh root@139.196.204.22 -p 22
+       sshpass -p  '*****' ssh root@139.196.204.22 -p 22
        ;;
     3) echo  "昂立web1"
-       sshpass -p  'Pzh6537.!@#' ssh root@139.196.98.231 -p 22
+       sshpass -p  '******' ssh root@139.196.98.231 -p 22
        ;;
     4) echo  "昂立web2"
-       sshpass -p  'Pzh6537.!@#' ssh root@139.196.92.195 -p 22       
+       sshpass -p  '******' ssh root@139.196.92.195 -p 22       
        ;;
     5) echo  "正式平台数据库"
-       sshpass -p  'hello2016!@#' ssh root@139.196.193.12 -p 22
+       sshpass -p  '******' ssh root@139.196.193.12 -p 22
        ;;  
     6)
        echo  "s-en.classba.cn    s-math.classba.cn" 
-       sshpass -p  'Xplus2015' ssh root@139.196.54.253 -p 22
+       sshpass -p  '*****' ssh root@139.196.54.253 -p 22
        ;;   
     H|h)
        cat <<MAYDAY
