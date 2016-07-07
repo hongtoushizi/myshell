4a4f055795c39ddd723549c561d90acb8b3537ce
hongtoushizi <yuanhongshuai3.11@163.com>
2016-07-07 11:34:46 +0800

    添加注释信息

--- centos/rabbitmq.sh
+++ centos/rabbitmq.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+echo "esl-erlang_18.3-1~centos~6_amd64.rpm"
 wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_18.3-1~centos~6_amd64.rpm
 
 #—————更新erlang—————#
+echo "更新erlang"
 wget http://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
 
-
+echo "安装erlang"
 rpm -ivh erlang-solutions-1.0-1.noarch.rpm 
 
 ll /etc/yum.repos.d/ | grep erl
@@ -16,6 +17,7 @@ cat /etc/yum.repos.d/erlang_solutions.repo
 yum install erlang
 
 #———————————————安装rabbitmq—————————————————————————————————————————————————#
+echo "安装rabbitmq"
 wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.1/rabbitmq-server-3.6.1-1.noarch.rpm
 
 
